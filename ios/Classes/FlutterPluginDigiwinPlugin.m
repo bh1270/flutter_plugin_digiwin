3b53b0477193b9f4acac2d922efe64700fbdccd2
hubei <hubei@sigo.cn>
2022-04-24 14:10:23 +0800

    11

---
+++ ios/Classes/FlutterPluginDigiwinPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterPluginDigiwinPlugin.h"
+
+@implementation FlutterPluginDigiwinPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_plugin_digiwin"
+            binaryMessenger:[registrar messenger]];
+  FlutterPluginDigiwinPlugin* instance = [[FlutterPluginDigiwinPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
---
+++ lib/flutter_plugin_digiwin.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+
+import 'package:flustars/flustars.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class FlutterPluginDigiwin {
+  static const MethodChannel _channel = MethodChannel('flutter_plugin_digiwin');
+
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+
+  static init() {
+    print('init');
+
+    LogUtil.init(tag: 'zzzz', maxLen: 100000,isDebug: true);
+  }
+
+  static void d(Object? object, {String? tag}) {
+    print('dddd');
+
+    LogUtil.d(object, tag: tag);
+  }
+}
